f9214f3b4411e59b1569db8b2eb8aefd264fa720
julianamschwand <julian_amschwand@sluz.ch>
2025-03-24 13:37:31 +0100

    Got template for API and installed dependencies

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const mysql = require("mysql2/promise");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+// MySQL-Verbindung
+const dbOptions = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+};
+
+const sessionStore = new MySQLStore(dbOptions);
+const dbConnection = mysql.createPool(dbOptions);
+
+// Session Middleware
+app.use(
+  session({
+    key: "session_id",
+    secret: process.env.SESSION_SECRET,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 },
+  })
+);
+
+
+
+const PORT = process.env.PORT || 3000; // Plesk gibt den Port vor
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
