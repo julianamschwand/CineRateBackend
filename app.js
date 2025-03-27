a3b692767a64e778e6945ee90bec06f5ca9e6db7
julianamschwand <julian_amschwand@sluz.ch>
2025-03-27 10:29:46 +0100

    Fixed the UserRole column to only contain user and admin and added on delete cascade restraints on foreign keys

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.use(
 
 
 
-const PORT = process.env.PORT || 3000; // Plesk gibt den Port vor
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
--- db.sql
+++ db.sql
@@ -3,15 +3,7 @@ create table UserData(
     Username varchar(30) unique,
     Email varchar(50) unique,
     UserPassword varchar(255),
-    UserRole varchar()
-);
-create table Comments(
-    CommentId int auto_increment primary key,
-    Content text,
-    fk_UserDataId int,
-    fk_MovieId int,
-    foreign key fk_UserDataId references UserData(UserDataId),
-    foreign key fk_MovieId references Movies(MovieId)
+    UserRole enum('admin', 'user') default 'user'
 );
 create table Movies(
     MovieId int auto_increment primary key,
@@ -20,14 +12,19 @@ create table Movies(
     PlaybackId varchar(11),
     Poster text
 );
-create table sessions(
-    SessionId int auto_increment primary key
+create table Comments(
+    CommentId int auto_increment primary key,
+    Content text,
+    fk_UserDataId int,
+    fk_MovieId int,
+    foreign key fk_UserDataId references UserData(UserDataId) on delete cascade,
+    foreign key fk_MovieId references Movies(MovieId) on delete cascade
 );
 create table Ratings(
     RatingId int auto_increment primary key,
     RatingValue decimal(2,1),
     fk_UserDataId int,
     fk_MovieId int,
-    foreign key fk_UserDataId references UserData(UserDataId),
-    foreign key fk_MovieId references Movies(MovieId)
+    foreign key fk_UserDataId references UserData(UserDataId) on delete cascade,
+    foreign key fk_MovieId references Movies(MovieId) on delete cascade
 );
