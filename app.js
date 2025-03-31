0451095fbe6d69f4476399d69901257e97491214
julianamschwand <julian_amschwand@sluz.ch>
2025-03-31 14:17:37 +0200

    Added login, userdata and logout function. The register function now logs the user in automatically

--- app.js
+++ app.js
@@ -36,21 +36,62 @@ app.use(
 app.post('/register', async (req, res) => {
   const {username, email, password} = req.body
   if (!username || !email || !password ) {
-    return res.status(400).json({error: "missing data"})
+    return res.status(400).json({error: "Missing data"})
   }       
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10)
     const result = await db.query("insert into UserData (Username, Email, UserPassword) values (?,?,?)", [username, email, hashedPassword])
+    req.session.user = { id: result.insertId, username: username, email: email }
     res.status(200).json({message: "User registered successfully", userId: result.insertId})
   } catch (error) {
+    console.error("Error:", error)
     res.status(500).json({message: "An Error happend while registering"})
   }
 })
 
-app.get('/ping', async (req, res) => {
-  return res.status(200).json({message: "Pong"})
+app.post('/login', async (req, res) => {
+  const {email, password} = req.body
+  if (!email || !password) {
+    return res.status(400).json({error: "Missing data"})
+  }
+
+  try {
+    const [results] = await db.query("select * from UserData where Email = ?", [email])
+    if (results.length === 0) {
+      return res.status(401).json({error: "No valid user"})
+    }
+    
+    const user = results[0]
+    const isPasswordValid = await bcrypt.compare(password, user.UserPassword)
+
+    if (!isPasswordValid) {
+      return res.status(401).json({error: "Wrong password"})
+    }
+    req.session.user = { id: user.UserDataId, username: user.Username, email: email }
+    res.status(200).json({message: "User successfully logged in"})
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({error: "An error happened while logging in"})
+  }
 })
+
+app.get('/userdata', async (req, res) => {
+  if (req.session.user) {
+    return res.status(200).json({id: req.session.user.id, username: req.session.user.username, email: req.session.user.email})
+  }
+  res.status(401).json({ message: 'Unauthorized' })
+})
+
+app.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+      if (err) {
+          return res.status(500).json({ message: 'Logout failed' })
+      }
+      res.clearCookie('SessionId')
+      res.json({ message: 'Logged out successfully' })
+  });
+});
 
 //////////////////////////////////////////
 
