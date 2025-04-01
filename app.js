20ca144ff45e54b8dc1141928282554dd5bd7733
julianamschwand <julian_amschwand@sluz.ch>
2025-04-01 16:53:42 +0200

    addmovie route and formatting for other routes

--- app.js
+++ app.js
@@ -35,10 +35,7 @@ app.use(
 
 app.post('/register', async (req, res) => {
   const {username, email, password} = req.body
-  if (!username || !email || !password ) {
-    return res.status(400).json({error: "Missing data"})
-  }       
-
+  if (!username || !email || !password ) return res.status(400).json({error: "Missing data"})      
   try {
     const hashedPassword = await bcrypt.hash(password, 10)
     const result = await db.query("insert into UserData (Username, Email, UserPassword) values (?,?,?)", [username, email, hashedPassword])
@@ -52,163 +49,198 @@ app.post('/register', async (req, res) => {
 
 app.post('/login', async (req, res) => {
   const {email, password} = req.body
-  if (!email || !password) {
-    return res.status(400).json({error: "Missing data"})
-  }
-
+  if (!email || !password) return res.status(400).json({error: "Missing data"})
   try {
-    const [results] = await db.query("select * from UserData where Email = ?", [email])
-    if (results.length === 0) {
-      return res.status(401).json({error: "No valid user"})
-    }
-    
-    const user = results[0]
-    const isPasswordValid = await bcrypt.compare(password, user.UserPassword)
+    let [user] = await db.query("select * from UserData where Email = ?", [email])
+    if (user.length === 0) return res.status(401).json({error: "Not a valid user"})
+    user = user[0]
 
-    if (!isPasswordValid) {
-      return res.status(401).json({error: "Wrong password"})
-    }
+    const isPasswordValid = await bcrypt.compare(password, user.UserPassword)
+    if (!isPasswordValid) return res.status(401).json({error: "Wrong password"})
+
     req.session.user = { id: user.UserDataId, username: user.Username, email: email }
     res.status(200).json({message: "User successfully logged in"})
   } catch (error) {
     console.error("Error:", error)
-    res.status(500).json({error: "An error happened while logging in"})
+    res.status(500).json({error: "Error while logging in"})
   }
 })
 
 app.get('/userdata', async (req, res) => {
-  if (req.session.user) {
-    try {
-      const [results] = await db.query("select * from UserData where UserDataId = ?", [req.session.user.id])
-      const user = results[0]
+  if (!req.session.user) return res.status(401).json({ message: 'Unauthorized' })
 
-      return res.status(200).json({id: user.UserDataId, username: user.Username, email: user.Email, role: user.UserRole})
-    } catch (error) {
-      console.error("Error:", error)
-      return res.status(500).json({error: "Error retrieving data from the database"})
-    }
+  try {
+    let [user] = await db.query("select * from UserData where UserDataId = ?", [req.session.user.id])
+    user = user[0]
+
+    res.status(200).json({id: user.UserDataId, username: user.Username, email: user.Email, role: user.UserRole})
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({error: "Error retrieving data from the database"})
   }
-  res.status(401).json({ message: 'Unauthorized' })
 })
 
 app.post('/logout', (req, res) => {
   req.session.destroy(err => {
-      if (err) {
-          return res.status(500).json({ message: 'Logout failed' })
-      }
+      if (err) return res.status(500).json({ message: 'Logout failed' })
       res.clearCookie('SessionId')
       res.json({ message: 'Logged out successfully' })
   })
 })
 
-app.post('/rolemod', async (req, res) => {
+app.post('/rolemod', async (req, resg) => {
   const {userdataid} = req.body
-  if (req.session.user) {
-    if (!userdataid) {
-      return res.status(400).json({message: "Missing data"})
-    }
+  if (!userdataid) return res.status(400).json({message: "Missing data"})
+  if (!req.session.user) return res.status(401).json({ message: 'Unauthorized' })
+
+  try {
+    let [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
+    user = user[0]
+    let [updatinguser] = await db.query("select UserRole, Username from UserData where UserDataId = ?", [userdataid])
+    if (updatinguser.length === 0) return res.status(401).json({error: "Not a valid user"})
+    updatinguser = updatinguser[0]
+
+    if (user.UserRole !== "admin" || updatinguser.UserRole === "admin") return res.status(403).json({error: "Forbidden"})
     
     try {
-      const [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
-      const [updatinguser] = await db.query("select UserRole, Username from UserData where UserDataId = ?", [userdataid])
-
-      if (user[0].UserRole === "admin" && updatinguser[0].UserRole !== "admin") {
-        try {
-          await db.query("update UserData set UserRole = 'mod' where UserDataId = ?", [userdataid])
-          return res.status(200).json({message: `Successfully made '${updatinguser[0].Username}' a moderator`})
-        } catch (error) {
-          console.error("Error:", error)
-          return res.status(500).json({error: "An error happened while updating the database"})
-        }
-      }
+      await db.query("update UserData set UserRole = 'mod' where UserDataId = ?", [userdataid])
+      res.status(200).json({message: `Successfully made '${updatinguser.Username}' a moderator`})
     } catch (error) {
-      return res.status(500).json({error: "An error happened while checking the users role"})
+      console.error("Error:", error)
+      res.status(500).json({error: "Error while updating the database"})
     }
-  } 
-  res.status(401).json({ message: 'Unauthorized' })
+    
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({error: "Error while checking the users role"})
+  }
 })
 
 app.post('/roleadmin', async (req, res) => {
   const {userdataid} = req.body
-  if (req.session.user) {
-    if (!userdataid) {
-      return res.status(400).json({message: "Missing data"})
-    }
-    
-    try {
-      const [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
-      const [updatinguser] = await db.query("select Username from UserData where UserDataId = ?", [userdataid])
+  if (!userdataid) return res.status(400).json({message: "Missing data"})
+  if (!req.session.user) return res.status(401).json({ message: 'Unauthorized' })
+ 
+  try {
+    let [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
+    user = user[0]
+    let [updatinguser] = await db.query("select Username from UserData where UserDataId = ?", [userdataid])
+    if (updatinguser.length === 0) return res.status(401).json({error: "Not a valid user"})
+    updatinguser = updatinguser[0]
 
-      if (user[0] === "admin") {
-        try {
-          await db.query("update UserData set UserRole = 'admin' where UserDataId = ?", [userdataid])
-          return res.status(200).json({message: `Successfully made '${updatinguser[0].Username}' an admin`})
-        } catch (error) {
-          console.error("Error:", error)
-          return res.status(500).json({error: "An error happened while updating the database"})
-        }
-      }
+    if (user[0] !== "admin") return res.status(403).json({error: "Forbidden"})
+
+    try {
+      await db.query("update UserData set UserRole = 'admin' where UserDataId = ?", [userdataid])
+      res.status(200).json({message: `Successfully made '${updatinguser[0].Username}' an admin`})
     } catch (error) {
-      return res.status(500).json({error: "An error happened while checking the users role"})
+      console.error("Error:", error)
+      res.status(500).json({error: "Error while updating the database"})
     }
-  } 
-  res.status(401).json({ message: 'Unauthorized' })
+  } catch (error) {
+    res.status(500).json({error: "Error while checking the users role"})
+  }
 })
 
 app.post('/roleuser', async (req, res) => {
   const {userdataid} = req.body
-  if (req.session.user) {
-    if (!userdataid) {
-      return res.status(400).json({message: "Missing data"})
+  if (!userdataid) return res.status(400).json({message: "Missing data"})
+  if (!req.session.user) return res.status(401).json({ message: 'Unauthorized' })
+
+  try {
+    let [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
+    user = user[0]
+    let [updatinguser] = await db.query("select UserRole, Username from UserData where UserDataId = ?", [userdataid])
+    if (updatinguser.length === 0) return res.status(401).json({error: "Not a valid user"})
+    updatinguser = updatinguser[0]
+
+    if (user.UserRole !== "admin" || updatinguser.UserRole === "admin") return res.status(403).json({error: "Forbidden"})
+
+    try {
+      await db.query("update UserData set UserRole = 'user' where UserDataId = ?", [userdataid])
+      res.status(200).json({message: `Successfully made '${updatinguser[0].Username}' a user`})
+    } catch (error) {
+      console.error("Error:", error)
+      res.status(500).json({error: "Error while updating the database"})
     }
     
-    try {
-      const [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
-      const [updatinguser] = await db.query("select UserRole, Username from UserData where UserDataId = ?", [userdataid])
-
-      if (user[0].UserRole === "admin" && updatinguser[0].UserRole !== "admin") {
-        try {
-          await db.query("update UserData set UserRole = 'user' where UserDataId = ?", [userdataid])
-          return res.status(200).json({message: `Successfully made '${updatinguser[0].Username}' a user`})
-        } catch (error) {
-          console.error("Error:", error)
-          return res.status(500).json({error: "An error happened while updating the database"})
-        }
-      }
-    } catch (error) {
-      return res.status(500).json({error: "An error happened while checking the users role"})
-    }
-  } 
-  res.status(401).json({ message: 'Unauthorized' })
+  } catch (error) {
+    res.status(500).json({error: "Error while checking the users role"})
+  }
 })
 
 app.post('/deleteuser', async (req, res) => {
   const {userdataid} = req.body
-  if (req.session.user) {
-    if (!userdataid) {
-      return res.status(400).json({message: "Missing data"})
-    }
+  if (!userdataid) return res.status(400).json({message: "Missing data"})
+  if (!req.session.user) return res.status(401).json({ message: 'Unauthorized' })
+
+  try {
+    let [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
+    user = user[0]
+    let [deletinguser] = await db.query("select UserRole, Username from UserData where UserDataId = ?", [userdataid])
+    if (deletinguser.length === 0) return res.status(401).json({error: "Not a valid user"})
+    deletinguser = deletinguser[0]
+
+    if (user[0].UserRole !== "admin" || deletinguser[0].UserRole === "admin") return res.status(403).json({error: "Forbidden"})
     
     try {
-      const [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
-      const [deletinguser] = await db.query("select UserRole, Username from UserData where UserDataId = ?", [userdataid])
-
-      if (user[0].UserRole === "admin" && deletinguser[0].UserRole !== "admin") {
-        try {
-          await db.query("delete from UserData where UserDataId = ?", [userdataid])
-          return res.status(200).json({message: `Successfully deleted '${deletinguser[0].Username}'`})
-        } catch (error) {
-          console.error("Error:", error)
-          return res.status(500).json({error: "An error happened while deleting from the database"})
-        }
-      }
+      await db.query("delete from UserData where UserDataId = ?", [userdataid])
+      res.status(200).json({message: `Successfully deleted '${deletinguser[0].Username}'`})
     } catch (error) {
-      return res.status(500).json({error: "An error happened while checking the users role"})
+      console.error("Error:", error)
+      res.status(500).json({error: "Error while deleting from the database"})
     }
-  } 
-  res.status(401).json({ message: 'Unauthorized' })
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({error: "Error while checking the users role"})
+  }
 })
 
+app.post('/addmovie', async (req, res) => {
+  const {title, description, poster, playbackid} = req.body 
+  if (!title || !description || !poster || !playbackid) return res.status(400).json({error: "Missing data"})
+  if (!req.session.user) return res.status(401).json({error: "Unauthorized"})
+  
+  try {
+    let [user] = await db.query("select UserRole from UserData where UserDataId = ?", [req.session.user.id])
+    user = user[0]
+
+    if (user.UserRole !== "admin" && user.UserRole !== "mod") return res.status(403).json({error: "Forbidden"})
+
+    try {
+      let [languagecodes] = await db.query("select LanguageCode from Languages")
+      languagecodes = languagecodes.map(lang => lang.LanguageCode)
+      try {
+        const [result] = await db.query("insert into Movies (PlaybackId, Poster) values (?,?)", [playbackid, poster])
+        const movieid = result.insertId
+
+        try {
+          for (const lang of languagecodes) {
+            if (title.hasOwnProperty(lang) && description.hasOwnProperty(lang)) {
+              let [languageid] = await db.query("select LanguageId from Languages where LanguageCode = ?", [lang])
+              languageid = languageid[0]
+              
+              await db.query("insert into MovieTranslations (Title, MovieDescription, fk_LanguageId, fk_MovieId) values (?,?,?,?)", [title[lang], description[lang], languageid[0].LanguageId, movieid])
+            }
+          }
+          res.status(200).json({message: "Successfully inserted the movie"})
+        } catch (error) {
+          console.error("Error:", error)
+          res.status(500).json({error: "Error while inserting the movie translations into the database"})
+        }
+      } catch (error) {
+        console.error("Error:", error)
+        res.status(500).json({error: "Error while inserting the movie into the database"})
+      }
+    } catch (error) {
+      console.error("Error:", error)
+      res.status(500).json({error: "Error while retrieving languages"})
+    }
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({error: "Error while checking the users role"})
+  }
+})
 
 //////////////////////////////////////////
 
