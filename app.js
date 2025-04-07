4faffe23c9cbbeee439b511f6c010236dcced023
julianamschwand <julian_amschwand@sluz.ch>
2025-04-07 12:16:54 +0200

    Filled in getrating and getcomments route. Replaced addrating and editrating by rate. Getrating and rate are tested, getcomments isn't.

--- app.js
+++ app.js
@@ -520,16 +520,58 @@ app.delete("/deletemovie", async (req, res) => {
 app.get("/getrating", async (req, res) => {
   const {movieid} = req.body
   if (!movieid) return res.status(400).json({success: false, error: "Missing data"})
+
+  try {
+    let [rating] = await db.query("select round(avg(RatingValue), 2) as 'Rating' from Ratings where fk_MovieId = ? group by RatingValue", [movieid])
+    if (rating.lenght === 0) return res.status(404).json({success: false, rating: "No rating"})
+    rating = rating[0].Rating
+
+    res.status(200).json({success: true, rating: rating})
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({success: false, error: "Error fetching rating"})
+  }
 })
 
 app.get("/getcomments", async (req, res) => {
   const {movieid} = req.body
   if (!movieid) return res.status(400).json({success: false, error: "Missing data"})
+
+  try {
+    const [comments] = await db.query("select CommentId, Content, (select Username from UserData where UserDataId = fk_UserDataId) as 'Username' from Comments") 
+    if (comments.length === 0) return res.status(404).json({success: false, rating: "No comments"})
+
+    res.status(200).json({success: true, comments: comments})
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({success: false, error: "Error fetching comments"})
+  }
 })
 
-app.post("/addrating", async (req,res) => {
-  const {movieid} = req.body
-  if (!movieid) return res.status(400).json({success: false, error: "Missing data"})
+app.post("/rate", async (req,res) => {
+  const {movieid, rating} = req.body
+  if (!movieid || !rating) return res.status(400).json({success: false, error: "Missing data"})
+  if (!req.session.user) return res.status(401).json({success: false, error: "Unauthorized"})
+
+  try {
+    const [ratings] = await db.query("select * from Ratings where fk_MovieId = ? and fk_UserDataId = ?", [movieid, req.session.user.id])
+
+    try {
+      if (ratings.length === 0) {
+        await db.query("insert into Ratings (RatingValue, fk_UserDataId, fk_MovieId) values (?,?,?)", [rating, req.session.user.id, movieid])
+      } else {
+        await db.query("update Ratings set RatingValue = ? where fk_UserDataId = ? and fk_MovieId = ?", [rating, req.session.user.id, movieid])
+      }
+
+      res.status(200).json({success: true, message: "Sucessfully rated the movie"})
+    } catch {
+      console.error("Error:", error)
+      res.status(500).json({success: false, error: "Error inserting / updating the rating"})
+    }
+  } catch (error) {
+    console.error("Error:", error)
+    res.status(500).json({success: false, error: "Error fetching ratings"})
+  }
 })
 
 app.post("/addcomment", async (req,res) => {
